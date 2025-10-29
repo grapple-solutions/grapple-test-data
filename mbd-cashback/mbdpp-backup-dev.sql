ed884f125541688af19ed889a0b5848d767bad16
Patrick Riegler <patrick.riegler@grapple-solutions.com>
2025-10-29 15:04:44 +0100

    fix typo in the user table creation - comment is only 2 dashes, 3 dashes fail in the mysql command

--- mbd-cashback/mbdpp-backup-dev.sql
+++ mbd-cashback/mbdpp-backup-dev.sql
@@ -1843,7 +1843,7 @@ CREATE TABLE `purchasereceipts_backup` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
---- Creating User tables
+-- Creating User tables
 
 CREATE TABLE `User` (
   `id` int NOT NULL AUTO_INCREMENT,
